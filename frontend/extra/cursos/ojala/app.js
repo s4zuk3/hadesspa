e41b2595e7c312c5f5df9bcc23294e31e64b8f5c
Francisco López Alarcón <francisco.lopez.a@usach.cl>
2016-08-01 02:01:02 -0400

    b

--- frontend/extra/cursos/ojala/app.js
+++ frontend/extra/cursos/ojala/app.js
@@ -1,10 +1,70 @@
 angular.module('ojalaApp', ['ngMaterial'])
 .controller('AppCtrl', function($scope,$http) {
-  $scope.imagePath = 'img/washedout.png';
+
   $scope.cursos = {};
-  $http.get("ojala.json")
+  $http.get("ojala.min.json")
     .then(function(response) {
         $scope.cursos = response.data;
     });
 });
 
+
+
+angular.module('ojalaApp2', ['ngMaterial'])
+  .controller('AppCtrl', function($scope) {
+    $scope.cursos = [{
+      "title":"asdf",
+      "category":"IOS",
+      "img":"/images/covers/excel-datesmedio.jpg"
+    },
+     {
+      "title":"asdf",
+      "category":"IOS22",
+      "img":"/images/covers/excel-datesmedio.jpg"
+    },
+                    {
+      "title":"asdf",
+      "category":"IOS22",
+      "img":"/images/covers/excel-datesmedio.jpg"
+    },{
+      "title":"asdf",
+      "category":"IOS22",
+      "img":"/images/covers/excel-datesmedio.jpg"
+    },{
+      "title":"asdf",
+      "category":"IOS22",
+      "img":"/images/covers/excel-datesmedio.jpg"
+    },{
+      "title":"asdf",
+      "category":"IOS22",
+      "img":"/images/covers/excel-datesmedio.jpg"
+    },{
+      "title":"asdf",
+      "category":"IOS22",
+      "img":"/images/covers/excel-datesmedio.jpg"
+    },{
+      "title":"asdf",
+      "category":"IOS22",
+      "img":"/images/covers/excel-datesmedio.jpg"
+    },{
+      "title":"asdf",
+      "category":"IOS22",
+      "img":"/images/covers/excel-datesmedio.jpg"
+    },{
+      "title":"asdf",
+      "category":"IOS22",
+      "img":"/images/covers/excel-datesmedio.jpg"
+    },{
+      "title":"asdf",
+      "category":"IOS22",
+      "img":"/images/covers/excel-datesmedio.jpg"
+    },{
+      "title":"asdf",
+      "category":"IOS22",
+      "img":"/images/covers/excel-datesmedio.jpg"
+    },{
+      "title":"asdf",
+      "category":"IOS22",
+      "img":"/images/covers/excel-datesmedio.jpg"
+    }]
+  });
